fcab986e6c53fb3f3e8c900da430dc404f450f8c
Ildar Lomov <ildar.lomov@ostrovok.ru>
2019-06-07 20:00:38 +0300

    Merge branch 'leaderboard' into f/cociecutter_init
    # Conflicts:
    #	eval.py

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 export CUDA_VISIBLE_DEVICES=0
 #python3 eval.py test_df.csv track_order_df.csv test_descriptors.npy agg_descriptors.npy
 #python eval.py data/raw/train_df.csv data/raw/train_df_track_order_df.csv data/raw/train_df_descriptors.npy models/baseline/agg_descriptors.npy
-python eval.py $1 $2 $3 $4
+python3 eval.py $1 $2 $3 $4
