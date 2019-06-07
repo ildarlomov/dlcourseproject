407d253a35a47241abeb6cb33335fc860dcade58
Ildar Lomov <ildar.lomov@ostrovok.ru>
2019-06-07 18:56:12 +0300

    cociecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='solution for machines can see competition',
+    author='Ildar Lomov',
+    license='MIT',
+)
