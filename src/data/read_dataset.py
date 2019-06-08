081626c467e09bc66d515ae0c381d68f1662cc58
Ildar Lomov <ildar.lomov@ostrovok.ru>
2019-06-08 14:39:51 +0300

    add freeze

--- src/data/read_dataset.py
+++ src/data/read_dataset.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
-from .preprocessing import ToTensor
+from preprocessing import ToTensor
 from tqdm import tqdm
 # Ignore warnings
 import warnings
